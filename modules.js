893a905f23f633b9d16792d231adf3d9fb94aa40
DaytonPlus <101413618+DaytonPlus@users.noreply.github.com>
2023-01-08 19:39:12 +0100

    fix json error
    json - JSON fix problem
    rename var json to jsn

--- modules.js
+++ modules.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const path = require("path");
 const { exec } = require("child_process");
 const zl = require("./modules/zip-lib");
-const { json } = require("stream/consumers");
 
 function add(name, back) {
   exec("npm install --save " + name, function (err, sout, serr, send) {
@@ -27,9 +26,9 @@ function clear(res) {
   var fpath = path.join(__dirname, "package.json");
   var data = fs.readFileSync(fpath, "utf8");
   if (data) {
-    var json = JSON.parse(data);
-    json.dependencies = {};
-    fs.writeFileSync(fpath, JSON.stringify(json));
+    var jsn = JSON.parse(data);
+    jsn.dependencies = {};
+    fs.writeFileSync(fpath, JSON.stringify(jsn));
     return true;
   }
   return false;
